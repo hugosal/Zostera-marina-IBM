fdff60b5956dc16261496e60629b8081e37985e0
hugosal <54606569+hugosal@users.noreply.github.com>
2020-04-07 17:03:51 -0700

    Update zostera_model_test_preparer.sh

--- zostera_model_test_preparer.sh
+++ zostera_model_test_preparer.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 echo "Zostera-marina-IBM model preparer"
-echo ""
-mkdir inputs
-mv sample_environment_2000.csv sample_founding_rhizomes_2000.csv sample_world.py inputs
-cd inputs
-python sample_world.py
-echo "Succes"
+if [ ! -d inputs/ ]; then 
+	mkdir inputs
+fi
+if [[ ! -f inputs/sample_environment_2000.csv && ! -f inputs/sample_founding_rhizomes_2000.csv && ! -f inputs/sample_world.py ]]; then
+	mv sample_environment_2000.csv sample_founding_rhizomes_2000.csv sample_world.py inputs
+	cd inputs
+	python sample_world.py
+fi
+echo "Success"
+dir
 exit 0
